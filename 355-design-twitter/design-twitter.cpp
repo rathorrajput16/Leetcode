ef9b3caf644bb91eb303fd049305baacee9a6a6b
Ayush <rathorrajput167@gmail.com>
2026-05-21 15:08:49 +0530

    Time: 4 ms (82.17%) | Memory: 56.1 MB (41.60%) - LeetSync

--- 355-design-twitter/design-twitter.cpp
+++ 355-design-twitter/design-twitter.cpp
@@ -6,7 +6,7 @@ public:
     Twitter() {
        time=0;
     }
-    priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>pq;
+   
     unordered_map<ll,set<ll>>follows;
     unordered_map<ll,vector<pair<ll,ll>>>tweets;
     void postTweet(int userId, int tweetId) {
@@ -15,13 +15,14 @@ public:
     }
     
     vector<int> getNewsFeed(int userId) {
-        for(int i=0;i<tweets[userId].size();i++){
+         priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>pq;
+        for(int i=max(0LL,(ll)(tweets[userId].size()-10));i<tweets[userId].size();i++){
             pq.push({tweets[userId][i].first,tweets[userId][i].second});
             if(pq.size()>10)pq.pop();
         }
         if(pq.size()>10)pq.pop();
         for(auto it:follows[userId]){
-             for(int i=0;i<tweets[it].size();i++){
+             for(int i=max(0LL,(ll)(tweets[it].size()-10));i<tweets[it].size();i++){
             pq.push({tweets[it][i].first,tweets[it][i].second});
             if(pq.size()>10)pq.pop();
         }
