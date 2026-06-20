b9f9bc3b25cb0089bc4dfd2878ba2378bcc31d7a
Ayush <rathorrajput167@gmail.com>
2026-06-20 18:19:28 +0530

    Time: 80 ms (16.06%) | Memory: 84.1 MB (17.88%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -15,20 +15,20 @@ vector<vector<string>>ans;
         }
         return true;
     }
-    void f(int ind,vector<string>&path,string s){
-          if(ind==s.size()){
+     void f(int ind,vector<string>&path,string s){
+        if(ind==s.size()){
             ans.push_back(path);
             return;
-          }
-          for(int i=ind;i<=s.size();i++){
-             if(ispalindrome(s.substr(ind,i-ind+1))){
+        }
+        for(int i=ind;i<s.size();i++){
+            if(ispalindrome(s.substr(ind,i-ind+1))){
                 path.push_back(s.substr(ind,i-ind+1));
                 f(i+1,path,s);
+
                 path.pop_back();
-             }
-          }
-    }
-    
+            }
+        }
+     }
     vector<vector<string>> partition(string s) {
             vector<string>path;
             f(0,path,s);
