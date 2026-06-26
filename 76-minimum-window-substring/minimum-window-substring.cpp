c0b073ee9aa8eaf6754905b24a7a1d65f32936bf
Ayush <rathorrajput167@gmail.com>
2026-06-27 01:18:29 +0530

    Time: 16 ms (29.58%) | Memory: 11.5 MB (64.53%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -2,31 +2,32 @@ class Solution {
     using ll=long long;
 public:
     string minWindow(string s, string t) {
-        if(t.size()>s.size())return "";
-        int start=-1;
-        int minlen=1e9;
-        int left=0;
         map<char,ll>mp;
-        for(int i=0;i<t.size();i++){
-             mp[t[i]]++;
-          }
-          int cnt=0;
-        for(int i=0;i<s.size();i++){
-            if(mp[s[i]]>0)cnt++;
-            mp[s[i]]--;
-            if(cnt==t.size()){
-                while(cnt==t.size()){
-                    if(minlen>i-left+1){
-                        minlen=i-left+1;
-                        start=left;
-                    }
-                    if(mp[s[left]]==0)cnt--;
-                    mp[s[left]]++;
-                    left++;
-                }
-            }
+        for(char ch:t){
+            mp[ch]++;
         }
-       if(start==-1) return "";
-        return s.substr(start,minlen);
+        int needed=0,left=0,start=0,ans=1e9;
+        for(int i=0;i<s.size();i++){
+            if(mp[s[i]]>0){    
+                needed++;
+            }
+            mp[s[i]]--;
+            while(needed==t.size()){
+                if(i-left+1<ans){
+                    start=left;
+                    ans=i-left+1;
+                }
+                 mp[s[left]]++;
+                 
+                 if(mp[s[left]]>0){
+                    needed--;
+                 }
+                 left++;
+        }
+        }
+        if (ans == 1e9)
+    return "";
+        string ans1=s.substr(start,ans);
+        return ans1;
     }
 };
