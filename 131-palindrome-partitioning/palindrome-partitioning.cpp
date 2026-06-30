8bce88d175ddc8387e5e85f39c6b16a58dc7e61d
Ayush <rathorrajput167@gmail.com>
2026-06-30 19:44:36 +0530

    Time: 78 ms (16.74%) | Memory: 84 MB (17.76%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,37 +1,34 @@
 class Solution {
 public:
-vector<vector<string>>ans;
-    bool ispalindrome(string s){
+    vector<vector<string>>ans;
+    bool ispallindrome(string s){
         int low=0;
         int high=s.size()-1;
         while(low<=high){
-            if(s[low]==s[high]){
-                low++;
-                high--;
+            if(s[low]!=s[high]){
+             return false;
             }
-            else{
-                return false;
-            }
+            low++;
+            high--;
         }
         return true;
     }
-     void f(int ind,vector<string>&path,string s){
+    void f(int ind,vector<string>&res,string s){
         if(ind==s.size()){
-            ans.push_back(path);
+            ans.push_back(res);
             return;
         }
         for(int i=ind;i<s.size();i++){
-            if(ispalindrome(s.substr(ind,i-ind+1))){
-                path.push_back(s.substr(ind,i-ind+1));
-                f(i+1,path,s);
-
-                path.pop_back();
+            if(ispallindrome(s.substr(ind,i-ind+1))){
+                res.push_back(s.substr(ind,i-ind+1));
+                f(i+1,res,s);
+                res.pop_back();
             }
         }
-     }
+    }
     vector<vector<string>> partition(string s) {
-            vector<string>path;
-            f(0,path,s);
-            return ans;
+      vector<string>res;
+       f(0,res,s);
+        return ans;
     }
 };
